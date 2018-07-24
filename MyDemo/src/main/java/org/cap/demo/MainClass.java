9a5e84a6716a20dde550e1aeecca9e38453e6425
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2018-07-24 14:27:06 +0530

    Commit All

---
+++ MyDemo/src/main/java/org/cap/demo/MainClass.java
@@ -0,0 +1,15 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		show();
+	}
+	
+	
+	public static void show() {
+		System.out.println("my Show Method");
+	}
+
+}
